a36de0d12fa5e096aa4ff493c3c0d42458f1c030
AleksandrGrabov <grabov-it@yandex.by>
2023-05-28 19:03:47 +0300

    Решили задачу 66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите значение M:");
+    int M = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите значение N:");
+    int N = int.Parse(Console.ReadLine());
+
+    int sum = GetNaturalSum(M, N);
+    Console.WriteLine("Сумма натуральных элементов: " + sum);
+
+   int GetNaturalSum(int start, int end)
+{
+    if (start > end)
+    {
+        return 0;
+    }
+    else
+    {
+        return start + GetNaturalSum(start + 1, end);
+    }
+}
