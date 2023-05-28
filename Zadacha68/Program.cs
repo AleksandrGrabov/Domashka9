8be0c0d9ddd87117b1015a8ec4ce0f6868ab1d4a
AleksandrGrabov <grabov-it@yandex.by>
2023-05-28 19:40:13 +0300

    Решили задачу 68

---
+++ Zadacha68/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите значение m:");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите значение n:");
+    int n = int.Parse(Console.ReadLine());
+
+    int result = AckermannFunction(m, n);
+    Console.WriteLine("Результат функции Аккермана: " + result);
+
+   int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
