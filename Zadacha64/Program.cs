ec051d853c7aaae3f5b76a380429d7b304f56035
AleksandrGrabov <grabov-it@yandex.by>
2023-05-28 18:55:43 +0300

    Решили задачу 64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,19 @@
+﻿ Console.WriteLine("Введите значение M:");
+    int M = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите значение N:");
+    int N = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(GetNaturalNumbers(M, N));
+
+   string GetNaturalNumbers(int current, int end)
+{
+    if (current == end)
+    {
+        return current.ToString();
+    }
+    else
+    {
+        return current.ToString() + ", " + GetNaturalNumbers(current + 1, end);
+    }
+}
